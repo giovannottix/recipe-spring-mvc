cf6b26924b8d2f59fa1337de978cfbb8ac83d255
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-10 18:47:16 -0500

    Add Category entity and set ManyToMany relationship with Recipe

---
+++ src/main/java/com/giovannottix/recipe/domain/Category.java
@@ -0,0 +1,45 @@
+package com.giovannottix.recipe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * @author: Giovanni Esposito.
+ * @Date : 05/10/20, Sun
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
