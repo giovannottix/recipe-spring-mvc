d8575561b6d333d279a1b078f241839faff9b189
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-10 18:02:07 -0500

    Add Difficulty Enum. Using EnumType String to store the string value in the database

--- src/main/java/com/giovannottix/recipe/domain/Recipe.java
+++ src/main/java/com/giovannottix/recipe/domain/Recipe.java
@@ -32,6 +32,9 @@ public class Recipe {
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     public Long getId() {
         return id;
     }
@@ -110,5 +113,21 @@ public class Recipe {
 
     public void setNote(Note note) {
         this.note = note;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
