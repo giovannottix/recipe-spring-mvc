0dc640133d5c4cd7a2fec4cd5d059488d76c1e5b
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-10 19:57:50 -0500

    Implement some Spring Data JPA query methods

--- src/main/java/com/giovannottix/recipe/controllers/IndexController.java
+++ src/main/java/com/giovannottix/recipe/controllers/IndexController.java
@@ -1,8 +1,14 @@
 package com.giovannottix.recipe.controllers;
 
+import com.giovannottix.recipe.domain.Category;
+import com.giovannottix.recipe.domain.UnitOfMeasure;
+import com.giovannottix.recipe.repositories.CategoryRepository;
+import com.giovannottix.recipe.repositories.UniOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 /**
  * @author: Giovanni Esposito.
@@ -11,9 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UniOfMeasureRepository uniOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository,
+                           UniOfMeasureRepository uniOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.uniOfMeasureRepository = uniOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "/index"})
     public String getIndex(Model model) {
-        System.out.println("Make a change 123");
+        Optional<Category> category =
+                this.categoryRepository.findByDescription("American");
+
+        Optional<UnitOfMeasure> unitOfMeasure =
+                this.uniOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is " + category.get().getId());
+        System.out.println("Unit id is " + unitOfMeasure.get().getId());
+
         return "index";
     }
 
--- src/main/java/com/giovannottix/recipe/repositories/CategoryRepository.java
+++ src/main/java/com/giovannottix/recipe/repositories/CategoryRepository.java
@@ -3,9 +3,14 @@ package com.giovannottix.recipe.repositories;
 import com.giovannottix.recipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 /**
  * @author: Giovanni Esposito.
  * @Date : 05/10/20, Sun
  */
-public interface CategoryRepo extends CrudRepository<Category, Long> {
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
