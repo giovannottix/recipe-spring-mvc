20500348925497a4d19363c7eff7079fdd7e5fc9
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-16 22:57:42 -0500

    Implement unit tests with JUnit

---
+++ src/test/java/com/giovannottix/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.giovannottix.recipe.services;
+
+import com.giovannottix.recipe.domain.Recipe;
+import com.giovannottix.recipe.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author: Giovanni Esposito.
+ * @Date : 05/16/20, Sat
+ */
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() throws Exception {
+        Recipe recipe = new Recipe();
+        Set<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+    }
+}
